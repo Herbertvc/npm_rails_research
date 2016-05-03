5f383113693888a84eae511f6146805f11bfb9e9
Herbert Videla <herbert.videla@tektonlabs.com>
2016-05-03 13:19:29 -0500

    not a Herbert commit

--- app.js
+++ app.js
@@ -4,16 +4,40 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nunjucks = require('express-nunjucks');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var music = require('./routes/music');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'templates'));
+
+// Templates in this directory will override the application templates. 
+// app.set('views', __dirname + '/templates');
+
+ 
+// Configuring the template system. 
+nunjucks.setup({
+    // (default: true) controls if output with dangerous characters are escaped automatically. 
+    autoescape: true,
+    // (default: false) throw errors when outputting a null/undefined value. 
+    throwOnUndefined: false,
+    // (default: false) automatically remove trailing newlines from a block/tag. 
+    trimBlocks: false,
+    // (default: false) automatically remove leading whitespace from a block/tag. 
+    lstripBlocks: false,
+    // (default: false) if true, the system will automatically update templates when they are changed on the filesystem. 
+    watch: true,
+    // (default: false) if true, the system will avoid using a cache and templates will be recompiled every single time. 
+    noCache: true,
+    // (default: see nunjucks syntax) defines the syntax for nunjucks tags. 
+    tags: {}
+}, app);
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,7 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/music',music);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var array = [1,2,3,4,5]
-  res.render('index', { title: 'Express', arr: array });
+  res.render('index.html', { title: '<p>Express</p>', arr: array });
 });
 
 module.exports = router;
